82257aab155ec6d79328f59a4457d4a50b39bbd2
SinkingDonut <abrar.gt@gmail.com>
2026-07-05 03:49:43 +0600

    Removed unused choice list and cleaned up the code a little

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 expenses = []
-cost = []
-choice = []
+costs = []
 while True:
     expense_name = input("Enter expense name (press Q to Quit): ")
     if expense_name.upper() == "Q":
         break
     expenses.append(expense_name)
     expense_cost = input("Enter cost of your expense: ")
-    cost.append(expense_cost)
+    costs.append(expense_cost)
 
     anotherexp = input("Do you want to add another expense?(y/n): ")
-    if anotherexp == "y":
-        choice.append(anotherexp)
-    elif anotherexp == "n":
+    if anotherexp == "n":
         break
 total = 0.00
-for item_cost in cost:
+for item_cost in costs:
     total += float(item_cost)
 print("\n--- Summary ---")
-print(f"Total spent: ${total:.2f}")
+print(f"Total spen      t: ${total:.2f}")
 
 
